d00c3cc1e9bf051969877291ff319dae055a9765
SciDroid <scidroidgames@gmail.com>
2021-11-15 05:26:45 +0000

    feat: :lipstick: Add layout component

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import 'tailwindcss/tailwind.css'
+import "tailwindcss/tailwind.css";
+import Layout from "components/Layout";
 
 const App = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
-}
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
+};
 
-export default App
+export default App;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
+import Link from "next/link";
+
 const Landing = () => {
-  return <h1>Hello world!</h1>;
+  return (
+    <h1>
+      <Link href="/api/docs">
+        <a>Check API</a>
+      </Link>
+    </h1>
+  );
 };
 
 export default Landing;
